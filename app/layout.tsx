5cf2aea46a1550d513029321cd855780bc7c6d50
vansh <vanshtesting@gmail.com>
2024-09-08 23:05:40 +0530

    removed login and logout verification.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-import LoginButton from './components/login'
-import LogoutButton from './components/logout'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
